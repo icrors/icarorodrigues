351e24819fc26b895dfa9060850c7c404e39939a
Ícaro Rodrigues <icro.rs@gmail.com>
2025-11-22 09:20:24 -0300

    adicionando primeiro exercicio

---
+++ primeiro.py
@@ -0,0 +1,14 @@
+lista = []
+primos = []
+nao_primos = []
+
+for i in range(1,11): lista.append(i)
+
+for n in lista:
+    divisores = 0
+
+    for i in range(1, n+1):
+        if n % i == 0: divisores += 1
+    
+    if divisores == 2: primos.append(n)
+    else: nao_primos.append(n)
